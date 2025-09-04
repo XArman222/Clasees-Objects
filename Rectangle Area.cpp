9d3676c0a21fc66742723b87c7084579eb11d5bc
XArman222 <xsoulop@gmail.com>
2025-09-04 09:50:00 +0530

    Rectangle Area.cpp

---
+++ Rectangle Area.cpp
@@ -0,0 +1,32 @@
+// Arman Majhi
+// 24070123022
+
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    double length;
+    double width;
+
+    void area() {
+        double result = length * width;
+        cout << "Area of Rectangle: " << result << endl;
+    }
+};
+
+int main() {
+    Rectangle r;
+    cout << "Enter length: ";
+    cin >> r.length;
+    cout << "Enter width: ";
+    cin >> r.width;
+
+    r.area();
+    return 0;
+}
+
+// OUTPUT
+// Enter length: 10
+// Enter width: 10
+// Area of Rectangle: 100
